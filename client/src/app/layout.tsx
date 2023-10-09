8628d1193eb01b6f95d8da35b52959fa1f3b5677
Ben Chavez <ben.m.chavez@gmail.com>
2023-10-08 23:12:19 -0500

    actual actaul start

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google';
 import './globals.css';
 import Navbar from '@/components/navbar';
+import { UserProvider } from '@auth0/nextjs-auth0/client';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -12,10 +13,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
-        <div className="flex min-h-screen flex-col items-center p-24">
-          {children}
-        </div>
+        <UserProvider>
+          <Navbar />
+          <div className="flex min-h-screen flex-col items-center p-24">
+            {children}
+          </div>
+        </UserProvider>
       </body>
     </html>
   );
--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -3,6 +3,7 @@
 
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
+import { useUser } from '@auth0/nextjs-auth0/client';
 
 // export type NavItem = {
 //   name: string;
@@ -23,17 +24,34 @@ const navItems = {
       href: '/private',
     },
   ],
+  anchorLinks: {
+    loginItem: {
+      name: 'Login',
+      href: '/api/auth/login',
+    },
+    logoutItem: {
+      name: 'Logout',
+      href: '/api/auth/logout',
+    },
+  },
 };
 
 export default function Navbar() {
   let pathname = usePathname();
+  const { user, isLoading } = useUser();
+
+  let authNavItem = navItems.anchorLinks.loginItem;
+
+  if (!isLoading && user) {
+    authNavItem = navItems.anchorLinks.logoutItem;
+  }
 
   return (
     <>
       <nav className="bg-gray-900">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-center mx-auto p-4">
           <div className="w-full md:block md:w-auto" id="navbar-default">
-            <div className="flex gap-8">
+            <div className="flex gap-8  items-center justify-center flex-row">
               {navItems.nextLinks.map((item) => {
                 return (
                   <Link
@@ -49,6 +67,13 @@ export default function Navbar() {
                   </Link>
                 );
               })}
+              <a
+                key={authNavItem.href}
+                href={authNavItem.href}
+                className="block rounded hover:opacity-70 text-white bg-pink-800/50 px-2 py-1"
+              >
+                {authNavItem.name}
+              </a>
             </div>
           </div>
         </div>
